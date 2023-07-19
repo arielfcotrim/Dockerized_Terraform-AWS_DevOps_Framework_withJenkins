b42e018a8e94b73d80b3865f19cdc571ab6d95f5
arielfcotrim <arielforner@gmail.com>
2023-07-19 19:45:19 +0300

    fix script [2]

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,37 +2,84 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 5.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "server" {
-  ami           = "ami-0c94855ba95c574c8"
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI (HVM), SSD Volume Type
   instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
               yum install -y docker
               service docker start
               usermod -a -G docker ec2-user
+              docker pull $DOCKER_USERNAME/$SERVER_IMAGE
               docker run -d -p 3000:3000 $DOCKER_USERNAME/$SERVER_IMAGE
               EOF
+
+  tags = {
+    Name = "Server"
+  }
 }
 
 resource "aws_instance" "frontend" {
-  ami           = "ami-0c94855ba95c574c8"
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI (HVM), SSD Volume Type
   instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
               yum install -y docker
               service docker start
               usermod -a -G docker ec2-user
+              docker pull $DOCKER_USERNAME/$FRONTEND_IMAGE
               docker run -d -p 3000:3000 $DOCKER_USERNAME/$FRONTEND_IMAGE
               EOF
+
+  tags = {
+    Name = "Frontend"
+  }
 }
