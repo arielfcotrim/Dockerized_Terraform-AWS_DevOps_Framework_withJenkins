374d88a2d66ddfc12b3f751808a79b7358bbfdc1
arielfcotrim <arielforner@gmail.com>
2023-07-26 19:18:42 +0300

    added mapping to pub ip add

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,6 +20,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_security_group" "allow_all" {
