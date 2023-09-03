70424247bf42a6657d1f0d0b34daf8d17aed33e6
arielfcotrim <arielforner@gmail.com>
2023-09-04 00:12:47 +0300

    return depends on

--- terraform/main.tf
+++ terraform/main.tf
@@ -231,6 +231,8 @@ resource "aws_instance" "frontend" {
   # Assign the custom security group to this instance
   vpc_security_group_ids = [aws_security_group.my_security_group.id]
 
+  depends_on = [data.aws_instance.backend_instance_data]
+
   # User data script to bootstrap the instance on startu
   user_data = <<-EOF
               #!/bin/bash
