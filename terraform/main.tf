0b5157e1f1ccbfd9eeb9d94aaed7fad1e8c708da
arielfcotrim <arielforner@gmail.com>
2023-09-05 21:14:10 +0300

    update ami id

--- terraform/main.tf
+++ terraform/main.tf
@@ -187,7 +187,7 @@ resource "aws_security_group_rule" "egress_all" {
 # Define the EC2 instance for the backend
 resource "aws_instance" "backend_instance" {
   # Specify the Amazon Machine Image ID
-  ami           = "ami-040d60c831d02d41c"
+  ami           = "ami-065681da47fb4e433"
   # Define the instance type
   instance_type = "t3.micro"
   # Define the SSH key for the instance
@@ -222,7 +222,7 @@ data "aws_instance" "backend_instance_data" {
 # Define the EC2 instance for the frontend
 resource "aws_instance" "frontend" {
   # Specify the Amazon Machine Image ID
-  ami           = "ami-040d60c831d02d41c"
+  ami           = "ami-065681da47fb4e433"
   # Define the instance type
   instance_type = "t3.micro"
   # Define the SSH key for the instance
